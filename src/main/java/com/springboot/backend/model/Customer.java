5ee9a3e8b87e457791c8ae01df56b4ff1c796023
Abel Garcia <1000075546@hexaware.com>
2022-07-18 16:12:44 -0500

    customer.java

--- src/main/java/com/springboot/backend/model/Customer.java
+++ src/main/java/com/springboot/backend/model/Customer.java
@@ -1,5 +1,71 @@
 package com.springboot.backend.model;
 
-public class Customer {
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
+@Entity
+public class Customer {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	
+	@Column(nullable = false)
+	private String name; 
+	
+	@Column(nullable = true)
+	private String password;
+	
+	private double balance;
+	
+	
+	
+	public Customer() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public Customer(Long id, String name, String password, double balance) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.password = password;
+		this.balance = balance;
+	}
+
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public double getBalance() {
+		return balance;
+	}
+	public void setBalance(double balance) {
+		this.balance = balance;
+	}
+
+	@Override
+	public String toString() {
+		return "Customer [id=" + id + ", name=" + name + ", password=" + password + ", balance=" + balance + "]";
+	}
+	
+	
+	
+	
 }
