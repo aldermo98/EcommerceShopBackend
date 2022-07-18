63934b413f8ca0b20805e6be6cc33d4ce12e8d0c
Abel Garcia <1000075546@hexaware.com>
2022-07-18 16:18:27 -0500

    basic implementations of customer controller/repository

--- src/main/java/com/springboot/backend/controller/CustomerController.java
+++ src/main/java/com/springboot/backend/controller/CustomerController.java
@@ -1,5 +1,11 @@
 package com.springboot.backend.controller;
 
+import org.springframework.web.bind.annotation.RestController;
+
+import com.springboot.backend.repository.CustomerRepository;
+
+@RestController
 public class CustomerController {
+	private CustomerRepository customerRepository;
 
 }
--- src/main/java/com/springboot/backend/repository/CustomerRepository.java
+++ src/main/java/com/springboot/backend/repository/CustomerRepository.java
@@ -1,5 +1,9 @@
 package com.springboot.backend.repository;
 
-public interface CustomerRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.springboot.backend.model.Customer;
+
+public interface CustomerRepository extends JpaRepository<Customer, Long>{
 
 }
