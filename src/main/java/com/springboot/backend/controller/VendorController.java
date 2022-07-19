8647c6390ca8394a809a2f42865e28e23ab959e4
Aayush Malde <1000075545@hexaware.com>
2022-07-19 14:34:47 -0400

    Vendor and controller

--- src/main/java/com/springboot/backend/controller/VendorController.java
+++ src/main/java/com/springboot/backend/controller/VendorController.java
@@ -1,5 +1,11 @@
 package com.springboot.backend.controller;
 
+import org.springframework.web.bind.annotation.RestController;
+
+import com.springboot.backend.repository.VendorRepository;
+
+@RestController
 public class VendorController {
+	private VendorRepository vendorRepository;
 
 }
--- src/main/java/com/springboot/backend/model/Vendor.java
+++ src/main/java/com/springboot/backend/model/Vendor.java
@@ -1,5 +1,81 @@
 package com.springboot.backend.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
 public class Vendor {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	
+	@Column(nullable = false)
+	private String vendorName; 
+	
+	@Column(nullable = true)
+	private String password;
+	
+	@Column(nullable = false)
+	private double balance;
+
+	public Vendor() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public Vendor(Long id, String vendorName, String password, Double balance) {
+		super();
+		this.id = id;
+		this.vendorName = vendorName;
+		this.password = password;
+		this.balance = balance;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getVendorName() {
+		return vendorName;
+	}
+
+	public void setVendorName(String vendorName) {
+		this.vendorName = vendorName;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public double getBalance() {
+		return balance;
+	}
+
+	public void setBalance(double balance) {
+		this.balance = balance;
+	}
+
+	@Override
+	public String toString() {
+		return "Vendor [id=" + id + ", vendorName=" + vendorName + ", password=" + password + ", balance=" + balance
+				+ "]";
+	}
+	
+	
+
+	
+	
 
 }
