4eb4ce0ae71d1e91cde2a8e4819083f6553a5494
Abel Garcia <abelg417@gmail.com>
2022-08-01 16:27:38 -0500

    small update on Customer controller

--- src/main/java/com/springboot/backend/controller/CustomerController.java
+++ src/main/java/com/springboot/backend/controller/CustomerController.java
@@ -4,12 +4,15 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.springboot.backend.model.Customer;
@@ -27,8 +30,17 @@ public class CustomerController {
 	}
 	
 	@GetMapping("/customers")
-	public List<Customer> getAllCustomers(){
-		return customerRepository.findAll();
+	public List<Customer> getAllCustomers(
+		@RequestParam("page") Integer page,
+		@RequestParam("size") Integer size) {
+	
+	if (page<0) {
+		page = 0;
+	}
+	
+	Pageable pageable=PageRequest.of(page, size);
+	
+	return customerRepository.findAll(pageable).getContent();
 	}
 	
 	@GetMapping("/customer/{id}")
